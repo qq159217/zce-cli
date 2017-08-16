3e4cc21aab97c29f3fe98d83dd55e4bb8017c06e
zce <w@zce.me>
2017-08-16 23:44:02 +0800

    Initial commit

--- test/unit/prompt.test.js
+++ test/unit/prompt.test.js
@@ -2,22 +2,15 @@ const assert = require('assert')
 const inquirer = require('inquirer')
 const prompt = require('../../lib/prompt')
 
-const prompts = {
-  name: { type: 'input', message: 'Project name' },
-  author: { type: 'input', message: 'Project author' },
-  version: { type: 'input', message: 'Project version' },
-  license: { type: 'input', message: 'Project license' },
-  repository: { type: 'input', message: 'Project repository' }
-}
-
 const patchInquirer = fake => {
   inquirer.prompt = questions => {
     const result = {}
     questions.forEach(question => {
       const key = question.name
       if (question.validate) {
-        const valid = question.validate(fake[key])
-        if (!valid) throw new Error(valid)
+        question.validate(fake[key])
+        // const valid = question.validate(fake[key])
+        // if (!valid) throw new Error(valid)
       }
       result[key] = fake[key] || question.default
     })
@@ -28,11 +21,21 @@ const patchInquirer = fake => {
 describe('lib/prompt', () => {
   describe('#prompt-basic', () => {
     it('Should return right answers', () => {
+      const prompts = {
+        name: { type: 'input', message: 'Project name' },
+        author: { type: 'input', message: 'Project author' },
+        version: { type: 'input', message: 'Project version' },
+        license: { type: 'input', message: 'Project license' },
+        repo: { type: 'input', message: 'Project repository' },
+        repository: { type: 'input', message: 'Project repository' }
+      }
+
       patchInquirer({
         name: 'foo',
         author: 'zce',
         version: '1.0.0',
         license: 'ISC',
+        repo: 'https://github.com/zce/zce-cli.git',
         repository: 'https://github.com/zce/zce-cli.git'
       })
 
@@ -41,13 +44,58 @@ describe('lib/prompt', () => {
         assert.equal('zce', answers.author)
         assert.equal('1.0.0', answers.version)
         assert.equal('ISC', answers.license)
+        assert.equal('https://github.com/zce/zce-cli.git', answers.repo)
         assert.equal('https://github.com/zce/zce-cli.git', answers.repository)
+      })
+    })
+  })
+
+  describe('#prompt-custom-validate', () => {
+    const prompts = {
+      name: {
+        type: 'input',
+        message: 'Project name',
+        validate: input => !!input
+      },
+      version: {
+        type: 'input',
+        message: 'Project version',
+        validate: input => !!input
+      }
+    }
+
+    it('Should return right answers', () => {
+      patchInquirer({
+        name: 'foo',
+        version: '1.0.0'
+      })
+
+      return prompt(prompts, __dirname).then(answers => {
+        assert.equal('foo', answers.name)
+        assert.equal('1.0.0', answers.version)
+      })
+    })
+
+    it('Should return right answers', () => {
+      patchInquirer({
+        name: 'FOO',
+        version: '1-0-0'
+      })
+
+      return prompt(prompts, __dirname).then(answers => {
+        assert.equal('FOO', answers.name)
+        assert.equal('1-0-0', answers.version)
       })
     })
   })
 
   describe('#prompt-default', () => {
     it('Should return right answers', () => {
+      const prompts = {
+        name: { type: 'input', message: 'Project name' },
+        repository: { type: 'input', message: 'Project repository' }
+      }
+
       patchInquirer({})
 
       return prompt(prompts, __dirname, true, 'foo').then(answers => {
