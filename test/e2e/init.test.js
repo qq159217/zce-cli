2ba1431c9af4f3e630ef215cfa6256e5161b63b8
zce <w@zce.me>
2017-08-17 00:24:36 +0800

    Initial commit

---
+++ test/e2e/init.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert')
+const init = require('../../lib/init')
+
+describe('lib/init', () => {
+  describe('#init-basic', () => {
+    it('WIP', () => {
+      assert.ok(true)
+    })
+  })
+})
---
+++ test/e2e/list.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert')
+const list = require('../../lib/list')
+
+describe('lib/list', () => {
+  describe('#list-basic', () => {
+    it('WIP', () => {
+      assert.ok(true)
+    })
+  })
+})
