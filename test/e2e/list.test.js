36058e07f01e8d164ce75c7f880ad66439547fbf
zce <w@zce.me>
2017-08-17 23:19:56 +0800

    Initial commit

--- test/e2e/list.test.js
+++ test/e2e/list.test.js
@@ -8,13 +8,27 @@ describe('lib/list', function () {
 
   describe('#list-basic', () => {
     it('Short return an array and log full list in console', () => {
-      return list().then(data => assert.ok(Array.isArray(data)))
+      return list()
+        .then(data => assert.ok(Array.isArray(data)))
+        .catch(err => assert.ok(!err))
     })
   })
 
   describe('#list-short', () => {
     it('Short return an array and log short list in console', () => {
-      return list(true).then(data => assert.ok(Array.isArray(data)))
+      return list(true)
+        .then(data => assert.ok(Array.isArray(data)))
+        .catch(err => assert.ok(!err))
+    })
+  })
+
+  describe('#list-empty', () => {
+    it('Short return an empty array and log `Not found available` in console', () => {
+      process.env.TEST_API = 'https://api.github.com/users/fake-users/repos'
+
+      return list(false, true)
+        .then(data => assert.ok(Array.isArray(data)))
+        .catch(err => assert.ok(!err))
     })
   })
 
